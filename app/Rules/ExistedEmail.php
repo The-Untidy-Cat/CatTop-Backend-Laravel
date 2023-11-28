6f6cdc3aab9483caa7e1dc9584f4f2bc615649ad
sheroanh <anhnguyentuan8@gmail.com>
2023-11-28 13:49:54 +0700

    [28/11] fix bugs

--- app/Rules/ExistedEmail.php
+++ app/Rules/ExistedEmail.php
@@ -2,6 +2,8 @@
 
 namespace App\Rules;
 
+use App\Enums\CustomerState;
+use App\Enums\EmployeeState;
 use App\Models\Customer;
 use App\Models\Employee;
 use Closure;
@@ -16,9 +18,9 @@ class ExistedEmail implements ValidationRule
      */
     public function validate(string $attribute, mixed $value, Closure $fail): void
     {
-        if (Customer::where([['email', '=', $value], ['status', '=', true]])->exists()) {
+        if (Customer::where([['email', '=', $value], ['state', '=', CustomerState::ACTIVE]])->exists()) {
             return;
-        } else if (Employee::where([['email', '=', $value], ['status', '=', true]])->exists()) {
+        } else if (Employee::where([['email', '=', $value], ['state', '=', EmployeeState::ACTIVE]])->exists()) {
             return;
         } else {
             $fail('validation.existed_email')->translate();
