eaa4e3c142243b7927e4e6a2f94ea5d16f2eb24b
sheroanh <anhnguyentuan8@gmail.com>
2023-12-10 15:45:14 +0700

    [10/12] fix bugs

--- app/Http/Controllers/Dashboard/UserController.php
+++ app/Http/Controllers/Dashboard/UserController.php
@@ -9,11 +9,14 @@ class UserController extends Controller
 {
     public function getProfile(Request $request)
     {
-        $user = $request->user()->employee();
+        $user = $request->user()->employee()->first()->only(['first_name', 'last_name', 'email', 'phone_number', 'state']);
         return response()->json([
             'code' => 200,
             'message' => __('messages.get.success', ['name' => 'profile']),
-            'data' => ['user' => $request->user()->employee()->first(['first_name', 'last_name', 'email', 'phone_number', 'state'])]
+            'data' => [
+                'user' =>
+                    [...$user, "username" => $request->user()->username],
+            ]
             // ->only(['id', 'first_name', 'last_name', 'email', 'phone_number', 'state'])
         ], 200);
     }
